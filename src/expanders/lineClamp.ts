814780f852210851ec25db6555cfb5d78decef5c
Kristóf Poduszló <kripod@protonmail.com>
2020-08-23 18:02:40 +0200

    refactor(lineClamp): types

--- src/expanders/lineClamp.ts
+++ src/expanders/lineClamp.ts
@@ -1,16 +1,15 @@
-import type { CSSProperties } from "../CSSProperties";
+import type { CSSProperties, CSSPropertiesWithValues } from "../CSSProperties";
 
-/* TODO: Replace "blockOverflow" with "blockEllipsis" (frenic/csstype#94) */
-
-export type LineClampLonghands = Required<Pick<CSSProperties, "maxLines">> & {
-	blockEllipsis: CSSProperties["blockOverflow"];
-} & /* TODO: Wrap inside `ConstantValuedCSSProperties` (frenic/csstype#111) */ {
-	continue: "discard";
-};
+export type LineClampLonghands = Required<
+	Pick<CSSProperties, "maxLines" | "blockEllipsis">
+> &
+	CSSPropertiesWithValues<{
+		continue: "discard";
+	}>;
 
 export function lineClamp(
 	maxLines: NonNullable<CSSProperties["maxLines"]>,
-	blockEllipsis: NonNullable<CSSProperties["blockOverflow"]> = "auto",
+	blockEllipsis: NonNullable<CSSProperties["blockEllipsis"]> = "auto",
 ): LineClampLonghands {
 	return {
 		maxLines,
