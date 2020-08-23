98c3382831ddb768e7899d983d795e8040d2f2a5
Kristóf Poduszló <kripod@protonmail.com>
2020-08-23 16:26:01 +0200

    refactor: restrict possible CSS value types

--- src/CSSProperties.ts
+++ src/CSSProperties.ts
@@ -1,8 +1,7 @@
 import type { Properties } from "csstype";
 
 export interface CSSProperties {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	[key: string]: any;
+	[key: string]: string | number;
 }
 
 export type CSSPropertiesWithValues<T extends Properties> = T &
