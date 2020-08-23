c5fa05ed0e9ab9d9bc415aaf338054511002c79c
Kristóf Poduszló <kripod@protonmail.com>
2020-08-23 17:44:22 +0200

    refactor: reduce imported type complexity

--- src/CSSProperties.ts
+++ src/CSSProperties.ts
@@ -1,9 +1,12 @@
-import type { Properties } from "csstype";
+import type { StandardLonghandProperties } from "csstype";
 
 export interface CSSProperties {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	[key: string]: any;
 }
 
-export type CSSPropertiesWithValues<T extends Properties> = T &
-	Pick<Properties, Extract<keyof T, keyof Properties>>;
+export type CSSPropertiesWithValues<T extends StandardLonghandProperties> = T &
+	Pick<
+		StandardLonghandProperties,
+		Extract<keyof T, keyof StandardLonghandProperties>
+	>;
