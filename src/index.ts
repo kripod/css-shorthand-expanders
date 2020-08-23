32f9d99a185532953b204bb98e8245befe5d23bf
Kristóf Poduszló <kripod@protonmail.com>
2020-08-23 16:18:52 +0200

    fix: add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
 export type { CSSProperties } from "css-shorthand-expanders";
+
+export { border } from "./border";
+export { borderColor } from "./borderColor";
+export { borderImage } from "./borderImage";
+export { borderStyle } from "./borderStyle";
+export { borderWidth } from "./borderWidth";
+export { flex } from "./flex";
+export { flexFlow } from "./flexFlow";
+export { gap } from "./gap";
+export { lineClamp } from "./lineClamp";
+export { listStyle } from "./listStyle";
+export { margin } from "./margin";
+export { overflow } from "./overflow";
+export { padding } from "./padding";
+export { placeContent } from "./placeContent";
+export { placeItems } from "./placeItems";
+export { placeSelf } from "./placeSelf";
