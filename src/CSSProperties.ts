77be52c15afefd99689fdc1b218f8b25b13fdc56
Kristóf Poduszló <kripod@protonmail.com>
2020-08-25 23:01:56 +0200

    refactor: improve constant-valued return types

--- src/CSSProperties.ts
+++ src/CSSProperties.ts
@@ -1,12 +1,13 @@
-import type { StandardLonghandProperties } from "csstype";
-
 export interface CSSProperties {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	[key: string]: any;
 }
 
-export type CSSPropertiesWithValues<T extends CSSProperties> = T &
-	Pick<
-		StandardLonghandProperties,
-		Extract<keyof T, keyof StandardLonghandProperties>
-	>;
+export type CSSPropertiesWithValues<T extends CSSProperties> =
+	// Retain the passed type
+	T &
+		// Inherit JSDoc from `CSSProperties`
+		Pick<
+			{ [P in keyof CSSProperties]-?: unknown },
+			Extract<keyof T, keyof CSSProperties>
+		>;
